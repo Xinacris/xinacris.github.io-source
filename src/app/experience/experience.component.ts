e6cae6bedf3b893e60a4b7d4000edd03ddcaac9b
Ali Mustafa Eren <meren@mngkargo.com.tr>
2022-09-09 10:02:40 +0300

    added experiences.json to make the site more automated

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.css'],
 })
 export class ExperienceComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  expUrl = '../assets/experiences.json';
+  public experiences: any = [];
 
   ngOnInit(): void {
+    this.http.get(this.expUrl).subscribe((res) => {
+      this.experiences.push(res);
+      console.log(this.experiences[0].experiences);
+    }),
+      (err: any) => {
+        console.log(err);
+      };
   }
-
 }
